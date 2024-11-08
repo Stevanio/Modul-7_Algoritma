b542279666ab8c738ed9a58de9be173c2861ee71
Stevanio <113884281+Stevanio@users.noreply.github.com>
2024-11-08 11:08:22 +0700

    Add files via upload

---
+++ Modul 7_Stevanio_065002200042_Laprak.py
@@ -0,0 +1,21 @@
+print('Ordinal Number')
+print('Ketik 0 untuk menghentikan program')
+def angka(nomor):
+    a = nomor % 10
+    if a == 1:
+        return nomor,'st'
+    elif a == 2:
+        return nomor,'nd'
+    elif a == 3:
+        return nomor,'rd'
+    elif a in range(4,21) or a == 0:
+        return nomor,'th'
+while True:
+    try:
+        nomor = abs(int(input('Masukkan angka: ')))
+        print(angka(nomor))
+        if nomor == 0:
+            print('Terima kasih telah menggunakan program saya')
+            break
+    except:
+        print('Error -> inputan harus dalam bentuk angka dan tidak kurang dari 0')
